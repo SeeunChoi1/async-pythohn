fade9c77948611629415f727fea4dd805b7de3f0
SeeunChoi1 <candy970106@gmail.com>
2022-09-03 23:45:35 +0900

    [add] 초기 환경세팅

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print('hello world')
+def main():
+    """method 생성"""
+    print("hello")
+
+
+print("hello world")
+
+"""
+초기 환경설정
+: python은 컴파일 언어가 아닌 인터프리터 언어라 초기 환경세팅이 굉장히 중요함
+ - Formatter : 언어가 비교적 유연하기 때문에 협업시 코딩 컨벤션을 맞추기 위함.
+ - Linter : 인터프리터 언어라 호출되지 않은 함수는 오류가 있어도 디버깅 시 알 수 없음. 이를 잡아낼 수 있게 하기 위한 툴.
+"""
